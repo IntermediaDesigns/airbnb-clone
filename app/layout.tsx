947558f7f8071758e16f0b159614b660ff769612
Lynjai Jimenez <intermediadesignsllc@gmail.com>
2024-07-19 16:40:34 -0400

    chore: Update RootLayout component to include navigation bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,17 @@
-import exp from 'constants';
-import './globals.css';
+import Navbar from '@/components/Navbar'
+import './globals.css'
 
-export default function RootLayout({ children }:{children:React.ReactNode}) {
-  return <html lang='en'>
-    <body>
-      <nav>Hello </nav>
-      {children}
-    </body>
-  </html>
+export default function RootLayout ({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang='en'>
+      <body>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  )
+}
